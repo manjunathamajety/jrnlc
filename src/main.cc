f650a7350c09d5eca7893d2b1a72572aa6c0eb80
wandering jedi <manjunathasarmamajety@gmail.com>
2025-12-31 08:44:31 +0530

    fix mistake with incorrect version >.<

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@
 #include <config.h>
 #include <parse.h>
 #include <help.h>
-#define VERSION "0.2.2"
+#define VERSION "0.2.1"
 int main(int argc,char** argv){
     try{        
         if(argc < 2){global_help(); return 0;}
